4c7f5e909a1687db5701ef84e2f02b9396ce5a0f
Brett <benderb73@gmail.com>
2019-04-12 18:13:38 -0400

    pop-up

--- game.js
+++ game.js
@@ -94,4 +94,17 @@ function reset() {
    
 }
 
+//Pop-Up
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
 });
